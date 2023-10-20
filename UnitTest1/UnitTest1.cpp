c694db003910eca71e837ba2408c55ba2c694b0e
nazar <nazar@BULKA>
2023-10-20 09:24:31 +0300

    Добавьте файлы проекта.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba5.1/1.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double p;
+			p = g(6, 7);
+			Assert::AreEqual(p, 0.0236598, 0.0000001);
+		}
+	};
+}
---
+++ laba5.1/1.cpp
@@ -0,0 +1,29 @@
+﻿//laba 5.1
+//variant 26
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double g(const double x, const double y); // Function prototype
+
+int main()
+{
+    double s, t;
+
+    cout << "s = ";
+    cin >> s;
+
+    cout << "t = ";
+    cin >> t;
+
+    double c = (g(s, t - 1) + pow(g(s, t), 2) * (t * t + s)) / (pow(g(s, t), 2) * (s * s + t * t));
+    cout << "c = " << c << endl;
+
+    return 0;
+}
+
+double g(const double x, const double y) // Function definition
+{
+    return (x / (y * y)) * sin(y / (x * x)); // Corrected the formula
+}
